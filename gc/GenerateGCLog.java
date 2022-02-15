82a27c3f9146530dbe5037cbd6a63eff03504a93
hechengdong <423233@nd.com>
2022-02-15 12:32:26 +0800

    GC模块：生成GC日志

---
+++ gc/GenerateGCLog.java
@@ -0,0 +1,85 @@
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * GC日志生成
+ * <p>
+ * java -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -Xms256m -Xmx256m GenerateGCLog duration=1
+ * -XX:+UseSerialGC
+ * -XX:+UseParralelGC
+ * -XX:+UseConcMarkSweepGC
+ * -XX:+UseG1GC
+ *
+ * @author Administrator
+ */
+public class GenerateGCLog {
+    private static final Random RANDOM = new Random();
+
+    public static void main(String[] args) {
+        // 运行时长参数
+        long duration = 1;
+        for (String arg : args) {
+            if (arg.startsWith("duration=")) {
+                String durationStr = arg.substring("duration=".length());
+                try {
+                    duration = Long.parseLong(durationStr);
+                } catch (Exception e) {
+                    // arg error. do nothing
+                }
+            }
+        }
+
+        // 根据运行时长计算结束时间戳
+        long endMillis = System.currentTimeMillis() + TimeUnit.SECONDS.toMillis(duration);
+
+        long counter = 0;
+        System.out.println("开始执行.......");
+        // 保存一部分对象，模拟进入老年代的长生命周期对象
+        int cacheSize = 2000;
+        Object[] cachedGarbage = new Object[cacheSize];
+        // 在此时间范围内，持续循环
+        while (System.currentTimeMillis() < endMillis) {
+            // 生成对象
+            Object garbage = generateGarbage(100 * 1024);
+            counter++;
+            int randomIndex = RANDOM.nextInt(2 * cacheSize);
+            // 随机保存对象
+            if (randomIndex < cacheSize) {
+                cachedGarbage[randomIndex] = garbage;
+            }
+        }
+        System.out.println("执行结束。共生成对象次数:" + counter);
+    }
+
+    /**
+     * 生成对象
+     */
+    private static Object generateGarbage(int maxSize) {
+        int randomSize = RANDOM.nextInt(maxSize);
+        int type = randomSize % 4;
+        Object result;
+        // 不同类型表示了不同大小的对象
+        switch (type) {
+            case 0:
+                result = new byte[randomSize];
+                break;
+            case 1:
+                result = new int[randomSize];
+                break;
+            case 2:
+                result = new double[randomSize];
+                break;
+            default:
+                StringBuilder builder = new StringBuilder();
+                String randomString = "randomString";
+                while (builder.length() < randomSize) {
+                    builder.append(randomString);
+                    builder.append(maxSize);
+                    builder.append(randomSize);
+                }
+                result = builder.toString();
+                break;
+        }
+        return result;
+    }
+}
---
+++ gc/collect_gc_log.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+gc_type_array=("Serial" "Parallel" "ConcMarkSweep" "G1")
+mem_array=(256 512 1024 2048 4096)
+duration_array=(1 5 10)
+times_array=({0..9})
+
+# gc类型
+for gc_type in ${gc_type_array[*]}
+do
+    # 堆内存
+    for mem in ${mem_array[*]}
+    do
+        # 运行时间
+        for duration in ${duration_array[*]}
+        do
+            # 执行次数
+            for a in ${times_array[*]}
+            do
+                java -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+Use${gc_type}GC -Xms${mem}m -Xmx${mem}m GenerateGCLog duration=${duration} >> gc_log_${gc_type}_${duration}_${mem}.txt 2>> gc_log_${gc_type}_${duration}_${mem}.txt
+            done
+        done
+    done
+done
